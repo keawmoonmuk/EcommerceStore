2386dd968db1cd03916e0eea6fb0da610ab9e71e
Keawmoonmuk <keawmoonmuk.k@gmail.com>
2021-05-13 20:38:29 +0700

    create api error handing

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,14 +1,13 @@
 
+using API.Extensions;
 using API.Helpers;
-using Core.Interfaces;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-//using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -24,29 +23,24 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();  //add repository patten
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>))); // add generic repo
+
             services.AddAutoMapper(typeof(MappingProfiles));  // add service automapper
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-            // services.AddSwaggerGen(c => 
-            // {
-            //     c.SwaggerDoc("v1", new OpenApifo {Title = "API",EnvironmentVariablesExtensions = "v1"});
-            // });
+            services.AddApplicationServices();  // add service application service
+            services.AddSwaggerDocumention();    // add swagger documetion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndPoint("/swagger/v1/swagger.json","API v1"));
+            app.UseMiddleware<ExceptionMiddleware>();
 
-            }
+            app.UseSwaggerDocumention();    // add use swagger documents
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");   //add api errror
 
             app.UseHttpsRedirection();
 
