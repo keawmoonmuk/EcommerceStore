6d7323faad512bf2ba8dc2271a2e10b0d83615cb
Keawmoonmuk <keawmoonmuk.k@gmail.com>
2021-05-24 12:08:07 +0700

    create clientAngular error-handling message

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';  //add module animation
-import {HttpClientModule} from '@angular/common/http';    //add http client
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';    //add http client
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';   //import navbar
-import { ShopModule } from './shop/shop.module';   //shop
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,9 @@ import { HomeModule } from './home/home.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
