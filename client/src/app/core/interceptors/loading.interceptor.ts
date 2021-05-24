931608f5e230f5254c0d6ea0f66555512635f4a4
Keawmoonmuk <keawmoonmuk.k@gmail.com>
2021-05-24 14:31:38 +0700

    create breadcrumbs pageheader loading indicator and waiting for data from api

---
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BusyService } from '../services/busy.service';
+import { delay, finalize } from 'rxjs/operators';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  //create constr BudyService
+  constructor(private busyServerice: BusyService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+   
+   this.busyServerice.busy();
+
+    return next.handle(request).pipe(
+      delay(1000),
+      finalize(() => {
+        this.busyServerice.idle();
+      })
+    )
+  }
+}
