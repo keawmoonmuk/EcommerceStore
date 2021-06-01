39f09f8bbe96b9f7e102fdfb7d459d709877fa66
Keawmoonmuk <keawmoonmuk.k@gmail.com>
2021-06-01 22:18:01 +0700

    create client identity by create -adding account feature  -forms in angular -reactive forms -resusable form component -client side validat -async validation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,14 +10,35 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Store';
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    // if (token) {
+    this.accountService.loadCurrentUser(token!)!.subscribe(() => {
+      console.log('loaded user');
+    }, error => {
+      console.log(error);
+    });
+  }
+  // }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
-      this.basketService.getBasket(basketId).subscribe(() => {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
           console.log('initialised basket');
-        }, error => {
+        },
+        (error) => {
           console.log(error);
         }
       );
